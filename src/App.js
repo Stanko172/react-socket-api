dbc1b641a25ac0d9fb341374a29e84b36e4043d7
Stanko Bebek <stanko.bebek82@gmail.com>
2020-03-07 22:42:39 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
 import './App.css';
+import {FaCarSide} from 'react-icons/fa';
+import {FaCircleNotch} from 'react-icons/fa';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    response: false,
+    isLoaded: false,
+    items: []
+  }
+
+  componentDidMount() {
+    fetch("http://smart.sum.ba/parking?withParkingSpaces=1")
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+          items: json,
+          isLoaded: true,
+      })
+    });
+
+    const socket = socketIOClient("http://smart.sum.ba/parking-events");
+    socket.on("parking-lot-state-change", data => this.setState({ response: data }));
+  }
+
+  render(){
+    var { items, isLoaded, response } = this.state;
+    console.log(response.id_parking_space);
+
+    if(isLoaded){
+      var socketDataResponse = items[0].parkingSpaces.map(i => {
+        if(i.id == response.id_parking_space){
+          console.log(response.occupied);
+          i.occupied = response.occupied;
+        }
+      })
+    }
+
+    if(!isLoaded){
+      return <div>Loading...</div>
+    }else{
+      return(
+        <div className="container">
+            { items[0].parkingSpaces.map(item =>(
+              <div key={item.id} className="card">
+              <div className="card__header">
+                <FaCarSide/>
+                <span>
+                {item.parking_space_name}
+                </span>
+                <hr />
+              </div>
+              <div className="card__body">
+                { item.occupied === 1 ? <FaCircleNotch color="red"/> : <FaCircleNotch color="green"/>}
+                <button>Rezerviraj</button>
+                <button>Detalji</button>
+              </div>
+              </div>
+            ))};
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
